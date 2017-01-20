7068fa5bf45e9fe9fd5a38ba852d7b468cd05d16
Lukasz Nawojowski <luknwpl@gmail.com>
2017-01-20 10:20:00 +0100

    Argh, why it made root inside the package...

---
+++ src/main/java/agh/cs/peacegatch/ChatWebSocketHandler.java
@@ -0,0 +1,40 @@
+package agh.cs.peacegatch;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+/**
+ * PeaceGatch
+ * Created by luknw on 17.01.2017
+ */
+
+@WebSocket
+public class ChatWebSocketHandler {
+
+    private String sender, msg;
+
+    @OnWebSocketConnect
+    public void onConnect(Session user) throws Exception {
+        String username = "User" + Chat.nextUserNumber++;
+        Chat.userUsernameMap.put(user, username);
+        Chat.broadcastMessage(sender = "Server", msg = (username + " joined the chat"));
+    }
+
+    @OnWebSocketClose
+    public void onClose(Session user, int statusCode, String reason) {
+        String username = Chat.userUsernameMap.get(user);
+        Chat.userUsernameMap.remove(user);
+        Chat.broadcastMessage(sender = "Server", msg = (username + " left the chat"));
+    }
+
+
+    @OnWebSocketMessage
+    public void onMessage(Session user, String message) {
+
+        Chat.broadcastMessage(sender = Chat.userUsernameMap.get(user), msg = message);
+    }
+
+}
